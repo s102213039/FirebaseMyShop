bdf24a6d45259800e7110a8cbf9807886ba0ef13
Y.Y. Chiu <s102213039@ncnu.edu.tw>
2020-03-01 16:59:19 +0800

    first push

--- app/src/main/java/com/yanli/myshop/MainActivity.kt
+++ app/src/main/java/com/yanli/myshop/MainActivity.kt
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        
+
+        //first push
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
